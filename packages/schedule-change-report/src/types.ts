60378d58e04ca135df9359471bdf8e40e5b811e9
Abeeref1 <vip.abeer2005@gmail.com>
2026-09-18 20:10:37 +0400

    Schedule trends: add types.ts

---
+++ packages/schedule-change-report/src/types.ts
@@ -0,0 +1,33 @@
+import type {
+  ActivityFieldChange,
+  RelationshipSignature,
+} from "../../schedule-revision-core/src";
+
+export interface ScheduleChangeActivityRow {
+  activityId: string;
+  changeKind: "added" | "removed" | "modified";
+  finishShiftDays: number | null;
+  floatShiftHours: number | null;
+  progressShiftPercent: number | null;
+  fieldChanges: ActivityFieldChange[];
+}
+
+export interface ScheduleChangeReportProjection {
+  schemaVersion: "1.0";
+  projectionKey: "schedule_change_report";
+  generatedAt: string;
+  producerVersion: string;
+  fromRevisionId: string;
+  toRevisionId: string;
+  matchedActivityCount: number;
+  populationMatchPercent: number | null;
+  addedActivityCount: number;
+  removedActivityCount: number;
+  modifiedActivityCount: number;
+  unchangedActivityCount: number;
+  addedRelationshipCount: number;
+  removedRelationshipCount: number;
+  addedRelationships: RelationshipSignature[];
+  removedRelationships: RelationshipSignature[];
+  changedActivities: ScheduleChangeActivityRow[];
+}
