6644203389f8b90116db6f14c0beafd587eb83d6
Abeeref1 <vip.abeer2005@gmail.com>
2026-09-18 18:34:27 +0400

    Fix contract parser export file formatting

--- packages/contract-parser/src/index.ts
+++ packages/contract-parser/src/index.ts
@@ -2,4 +2,5 @@ export * from "./types";
 export * from "./numbering";
 export * from "./segmenter";
 export * from "./parser";
-export * from "./docx";\nexport * from "./ai";\n
+export * from "./docx";
+export * from "./ai";
