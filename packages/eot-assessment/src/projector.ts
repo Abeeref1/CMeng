f080e425743dafbbee1f3f8dfdd901e792db3553
Abeeref1 <vip.abeer2005@gmail.com>
2026-09-20 18:43:45 +0400

    fix: publish EOT determination population in assessment

--- packages/eot-assessment/src/projector.ts
+++ packages/eot-assessment/src/projector.ts
@@ -497,6 +497,9 @@ export function buildEotAssessmentProjection(
     ...(contractTime.overlapResolution ? {timeBasisReconciliation: {
       incorporatedEotDays: contractTime.incorporatedEotDays ?? null,
       registerDeterminationDays: contractTime.registerDeterminationDays ?? null,
+      registerDeterminationCount: contractTime.registerDeterminationCount ?? null,
+      effectiveDeterminationCount: contractTime.effectiveDeterminationCount ?? null,
+      futureDeterminationCount: contractTime.futureDeterminationCount ?? null,
       additionalApprovedEotDays: additionalAwardDays,
       overlapResolution: contractTime.overlapResolution,
       dataDateIso: contractTime.dataDateIso ?? null,
